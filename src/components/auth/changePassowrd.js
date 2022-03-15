a0879b9f11959671d2e1b095de1e55f88a32d0ed
Gurvijay <gillgurvijay01@gmail.com>
2022-03-15 21:50:12 +0530

    worked on signup trrying on changepassword

--- src/components/auth/changePassowrd.js
+++ src/components/auth/changePassowrd.js
@@ -1,32 +1,57 @@
-function ChangePassword() {
-    return (
-        <div className="container-fluid pt-4 px-4">
-            <div className="row g-4">
-            <div className="col-12">
-                <div className="bg-light rounded h-100 p-4">
-                <h6 className="mb-4">Create User</h6>
-                <form>
-                    <div className="mb-3">
-                    <label htmlFor="oldPassword" className="form-label">Password</label>
-                    <input type="password" name="oldPassword" className="form-control" id="oldPassword" />
-                    <i className="fa fa-eye" />
+import React from "react";
+class ChangePassword extends React.Component {
+    constructor() {
+        super();
+        this.state = { oldPassword:'password', passwordType: 'password', confirm: 'password'};
+      }
+
+      showPasswordOld(old) {
+        old.target.classList.toggle('fa-eye-slash')
+        var oldPassword = this.state.oldPassword === 'password' ? 'text' : 'password'
+        this.setState({oldPassword:oldPassword})
+    }
+    
+    showPassword(e) {
+        e.target.classList.toggle('fa-eye-slash')
+        var passwordType = this.state.passwordType === 'password' ? 'text' : 'password'
+        this.setState({passwordType:passwordType})
+    }
+
+    showPasswordConf(f) {
+        f.target.classList.toggle('fa-eye-slash')
+        var confirm = this.state.confirm === 'password' ? 'text' : 'password'
+        this.setState({confirm:confirm})
+    }
+    render() {
+        return (
+            <div className="container-fluid pt-4 px-4">
+                <div className="row g-4">
+                    <div className="col-12">
+                        <div className="bg-light rounded h-100 p-4">
+                            <h6 className="mb-4">Create User</h6>
+                            <form>
+                                <div className="mb-3">
+                                    <label htmlFor="oldPassword" className="form-label">Password</label>
+                                    <input type={this.state.oldPassword} name="oldPassword" className="form-control" id="oldPassword" />
+                                    <i className="fa fa-eye" onClick={(old)=>this.showPasswordOld(old)}></i>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="newPassword" className="form-label">Password</label>
+                                    <input type={this.state.passwordType} name="newPassword" className="form-control" id="newPassword" />
+                                    <i className="fa fa-eye" onClick={(e)=>this.showPassword(e)}></i>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="currentPassword" className="form-label">Password</label>
+                                    <input type={this.state.confirm} name="currentPassword" className="form-control" id="currentPassword" />
+                                    <i className="fa fa-eye" onClick={(f)=>this.showPasswordConf(f)}></i>
+                                </div>
+                                <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
+                        </div>
                     </div>
-                    <div className="mb-3">
-                    <label htmlFor="newPassword" className="form-label">Password</label>
-                    <input type="password" name="newPassword" className="form-control" id="newPassword" />
-                    <i className="fa fa-eye" />
-                    </div>
-                    <div className="mb-3">
-                    <label htmlFor="currentPassword" className="form-label">Password</label>
-                    <input type="password" name="currentPassword" className="form-control" id="currentPassword" />
-                    <i className="fa fa-eye" />
-                    </div>
-                    <button type="submit" className="btn btn-primary">Submit</button>
-                </form>
                 </div>
             </div>
-            </div>
-        </div>
-    );
+        );
+    }
 }
 export default ChangePassword;
--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,24 +1,36 @@
-function SignUp() {
-    return (
+import React from "react";
+class SignUp extends React.Component {
+    constructor() {
+        super();
+        this.state = { passwordType: 'password'};
+      }
+
+    showPassword(e) {
+        e.target.classList.toggle('fa-eye-slash')
+        var passwordType = this.state.passwordType === 'password' ? 'text' : 'password'
+        this.setState({passwordType:passwordType})
+    }
+    render() {
+        return (
             <div className="container-fluid">
-                <div className="row h-100 align-items-center justify-content-center" style={{minHeight: "100vh"}}>
+                <div className="row h-100 align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
                     <div className="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
                         <div className="bg-light rounded p-4 p-sm-5 my-4 mx-3">
                             <div className="d-flex align-items-center justify-content-between mb-3">
                                 <h3>Sign Up</h3>
                             </div>
                             <div className="form-floating mb-3">
-                                <input type="text" className="form-control" id="floatingText" placeholder="jhondoe"/>
+                                <input type="text" className="form-control" id="floatingText" placeholder="jhondoe" />
                                 <label for="floatingText">Username</label>
                             </div>
                             <div className="form-floating mb-3">
-                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
                                 <label for="floatingInput">Email address</label>
                             </div>
                             <div className="form-floating mb-4">
-                                <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+                                <input type={this.state.passwordType} className="form-control" id="floatingPassword" placeholder="Password" />
                                 <label for="floatingPassword">Password</label>
-                                <i className="fa fa-eye" style={{bottom:"35px !important"}}></i>
+                                <i className="fa fa-eye" onClick={(e)=>this.showPassword(e)} style={{ bottom: "35px !important" }}></i>
                             </div>
                             <button type="submit" className="btn btn-primary py-3 w-100 mb-4">Sign Up</button>
                             <p className="text-center mb-0">Already have an Account? <a href="/">Sign In</a></p>
@@ -26,7 +38,8 @@ function SignUp() {
                     </div>
                 </div>
             </div>
-        );  
-}
+        );
+    }
 
+}
 export default SignUp;
